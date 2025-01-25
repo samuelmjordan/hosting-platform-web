17e1ace8bfde9d093f828ed955341e8a0646d583
Samuel Jordan <87397295+jordan12sam@users.noreply.github.com>
2025-01-25 23:42:39 +0000

    checkout return page

--- src/app/(core)/store/page.tsx
+++ src/app/(core)/store/page.tsx
@@ -4,6 +4,9 @@ import { Card } from "@/components/ui/card"
 export default function Store() {
   return (
     <div className="container min-h-screen mx-auto sm:py-24">
+        <h1 className="text-6xl font-extrabold tracking-tight text-center sm:text-8xl lg:text-9xl mb-6">
+          Store
+        </h1>
       <Card>
         <StoreComponent />
       </Card>
