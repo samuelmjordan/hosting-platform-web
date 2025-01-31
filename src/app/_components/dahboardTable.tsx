6a94a4a7fdeb0270517b31b01c6d0b7067a44a30
Samuel Jordan <87397295+jordan12sam@users.noreply.github.com>
2025-01-31 22:47:06 +0000

    dashboard rework

--- src/app/_components/dahboardTable.tsx
+++ src/app/_components/dahboardTable.tsx
@@ -13,7 +13,6 @@ import { EllipsisVertical } from 'lucide-react';
 const getStatusStyles = (status: string) => {
   switch (status.toLowerCase()) {
     case "online":
-      console.log("here");
       return 'bg-green-200';
     case "offline":
       return 'bg-red-200';
@@ -40,14 +39,17 @@ export function DashboardTable({
             </TableHeader>
             <TableBody>
               {servers.map((server) => 
-              <TableRow>
+              <TableRow key={server.id}>
                 <TableCell>
                   <div>
                     <h1 className="text-2xl font-bold mb-3">{server.title}</h1>
-                    <p className="text-gray-600 mb-4">{server.description}</p>
+                    <p className="text-gray-600 mb-4 max-w-s line-clamp-3">{server.description}</p>
                     <div className="flex items-center gap-3">
-                      <span className={`inline-flex px-3 py-1 text-sm rounded-full ${getStatusStyles(server.status)} font-semibold`}>
+                      <span className={`inline-flex px-3 py-1 text-sm rounded-full ${getStatusStyles(server.status)} font-semibold min-w-20 justify-center`}>
                         {server.status}
+                      </span>
+                      <span className={`inline-flex px-3 py-1 text-sm rounded-full bg-gray-100 font-semibold min-w-10 justify-center`}>
+                        5/20
                       </span>
                       <span className="text-sm text-gray-600">Last online 2 hours ago</span>
                     </div>
