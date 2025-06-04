2786b4f94fb0a8af3bc5fa254708597ffc3e2354
Samuel Jordan <87397295+jordan12sam@users.noreply.github.com>
2025-06-04 18:06:45 +0100

    spec endpoint

--- src/app/api/user/subscription/actions/route.ts
+++ src/app/api/user/subscription/actions/route.ts
@@ -8,13 +8,16 @@ export async function POST(request: Request) {
     return NextResponse.json({ error: "Not authenticated" }, { status: 401 });
   }
 
-  const { title, address, region, action, subscriptionId } = await request.json();
-  const basePath = `${process.env.API_URL}/api/stripe/user/${userId}/subscription/${subscriptionId}`
+  const { title, address, region, action, subscriptionId, specificationId } = await request.json();
+  const stripeBasePath = `${process.env.API_URL}/api/stripe/user/${userId}/subscription/${subscriptionId}`
+  const basePath = `${process.env.API_URL}/api/user/${userId}/subscription/${subscriptionId}`
+
+  console.log({ title, address, region, action, subscriptionId })
 
   try {
     switch (action) {
       case "cancel":
-        const createResponse = await fetch(`${basePath}/cancel`, {
+        const createResponse = await fetch(`${stripeBasePath}/cancel`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
         });
@@ -27,13 +30,26 @@ export async function POST(request: Request) {
         return NextResponse.json({ url });
 
       case "uncancel":
-        const setDefaultResponse = await fetch(`${basePath}/uncancel`, {
+        const setDefaultResponse = await fetch(`${stripeBasePath}/uncancel`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' }
         });
         
         if (!setDefaultResponse.ok) {
           throw new Error('Failed to unancel subscription');
+        }
+        
+        return NextResponse.json({ success: true });
+
+      case "change-specification":
+        const specificationResponse = await fetch(`${stripeBasePath}/specification`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ specificationId })
+        });
+        
+        if (!specificationResponse.ok) {
+          throw new Error('Failed to change subscription specification');
         }
         
         return NextResponse.json({ success: true });
